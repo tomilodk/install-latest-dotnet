1b975c01924a601e0e0132dedc38136408e164b9
Milo <tomilobas@gmail.com>
2024-07-12 11:45:52 +0200

    Open latest installed sdk

--- install.js
+++ install.js
@@ -1,3 +1,7 @@
+const path = require('path');
+const os = require('os');
+const fs = require('fs').promises;
+const exec = require('child_process').exec;
 const puppeteer = require('puppeteer');
 
 async function run() {
@@ -5,9 +9,9 @@ async function run() {
     // Launch the browser and open a new blank page
     //Set useragent to be mac
 
-    const browser = await puppeteer.launch({headless: true});
+    const browser = await puppeteer.launch({ headless: true });
 
-    browser.userAgent = async ()=> "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36";
+    browser.userAgent = async () => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36";
 
     const page = await browser.newPage();
 
@@ -32,6 +36,18 @@ async function run() {
     await new Promise(resolve => setTimeout(resolve, 10000));
 
     await browser.close();
-} 
 
+    await new Promise(resolve => setTimeout(resolve, 10000));
+
+    //Get latest "dotnet-sdk*" file from Dwonloads folder
+    const downloadsFolder = path.join(os.homedir(), 'Downloads');
+    const latestSdk = await fs.readdir(downloadsFolder)
+        .then(files => files.filter(file => file.startsWith('dotnet-sdk-')))
+        .then(files => files.sort((a, b) => a.localeCompare(b))[0]);
+
+    //Luanch the file
+    const dotnetSdkPath = path.join(downloadsFolder, latestSdk);
+    console.log(dotnetSdkPath);
+    exec(`open "${dotnetSdkPath}"`);
+}
 run();
